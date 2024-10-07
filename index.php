8121459f6a8ccb30b47914b3cca5bff681bd05a9
Bitrix user <bitrix@localhost.localdomain>
2024-10-07 14:08:45 +0000

    add news in index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Test");
 ?>
 <div class="index-incareas-container">
-<?for($i = 0; $i < 3; $i++){?>
-<?$APPLICATION->IncludeComponent("bitrix:main.include","grey_incarea",Array(
+<?for ($i = 0; $i < 3; $i++){?>
+<?$APPLICATION->IncludeComponent("bitrix:main.include","grey_incarea", Array(
 	"AREA_FILE_SHOW" => "page", 
 	"AREA_FILE_SUFFIX" => "inc" . $i, 
 	"AREA_FILE_RECURSIVE" => "N",
@@ -12,4 +12,9 @@ $APPLICATION->SetTitle("Test");
 );?>
 <?}?>
 </div>
+<?$APPLICATION->IncludeComponent('my_news', 'news_single', [
+	'IBLOCK_ID' => 22, 
+	'arNavStartParams' => ['nTopCount' => 3],
+]	
+);?>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
